62be8cb90e02863e5d78f820ca8e48f8727d5120
Vladyslav Pekker <agilesteel@gmail.com>
2020-07-30 15:41:55 -0300

    ...

--- tools/ci/core/list-dependencies.sh
+++ tools/ci/core/list-dependencies.sh
@@ -42,6 +42,7 @@ for PROJECT in $(${DIR}/list-projects.sh); do
     DEPENDENCIES_FILE="$DIR/../../../$PROJECT/$CI_DEPENDENCIES_FILE"
     if [[ -f $DEPENDENCIES_FILE ]]; then
         for INCLUDE in $(cat $DEPENDENCIES_FILE); do
+            INCLUDE=$(realpath --relative-to="$DIR/../../.." "$INCLUDE")
             echo "$PROJECT $INCLUDE"
         done
     fi
