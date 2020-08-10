7eb83d6a60f330ef60144ebd1cb45c417b04860c
Vladyslav Pekker <agilesteel@gmail.com>
2020-08-10 11:07:51 -0300

    ...

--- tools/ci/core/build.sh
+++ tools/ci/core/build.sh
@@ -54,6 +54,9 @@ fi
 echo "Following projects need to be built"
 echo -e "$PROJECTS_TO_BUILD"
 
+mkdir -p ~/workspace
+echo "export PROJECTS_WITH_CHANGES=$PROJECTS_TO_BUILD" >> ~/workspace/projects-with-changes
+
 # Build all modified projects
 # echo -e "$PROJECTS_TO_BUILD" | while read PROJECTS; do
 #     CI_PLUGIN=${CI_PLUGIN} $DIR/build-projects.sh ${PROJECTS}
